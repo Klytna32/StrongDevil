7c11c7453979835f109fd9618c24690b08a8ef51
Klytna32 <strongdevilbryansk@yandex.ru>
2025-01-09 19:19:36 +0500

    Class raschetBuchla

---
+++ src/main/java/org/example/springin/RaschetCheny.java
@@ -0,0 +1,8 @@
+package org.example.springin;
+
+public class RaschetCheny {
+    public double raschetCheny(double vesBuchla,double chena){
+       double chisloButylok=vesBuchla/500;
+       double stoimost=chisloButylok*chena;
+    }
+}
